752b7928542c8b79cef6102749f83883dbe8b605
Haicheng Charles Zhao <czhao39@gmail.com>
2017-03-31 21:51:57 -0400

    tweak lost detector

--- scripts/lost_detector.py
+++ scripts/lost_detector.py
@@ -11,17 +11,16 @@ class LostDetector:
         self.poses_sub = rospy.Subscriber("particlecloud", PoseArray, self.poses_cb)
         self.is_lost = False
         self.msg = Bool()
+        self.lost_start = rospy.get_time()
     
     def poses_cb(self, msg):
         if len(msg.poses) > 1500:  # lost
             if not self.is_lost:
                 self.is_lost = True
                 self.lost_start = rospy.get_time()
-            else:
-                # Publish that the car is lost if it's been lost for 5 seconds
-                if rospy.get_time() - self.lost_start > 5.0:
-                    self.msg.data = True
-                    self.lost_pub.publish(self.msg)
+            elif rospy.get_time() - self.lost_start > 5.0:  # Publish that the car is lost if it's been lost for 5 seconds
+                self.msg.data = True
+                self.lost_pub.publish(self.msg)
         elif self.is_lost:
             self.is_lost = False
             self.msg.data = False
