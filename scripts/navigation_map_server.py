35cba159904e6bb5e942dd7b8d0b612189b63c0a
Haicheng Charles Zhao <czhao39@gmail.com>
2017-04-24 18:29:43 -0400

    save updated map to disk every 5 updates

--- scripts/navigation_map_server.py
+++ scripts/navigation_map_server.py
@@ -5,6 +5,7 @@ from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import Bool
 
 
+
 class NavigationMapServer:
     """
     This node publishes altered maps used for navigation.
@@ -12,12 +13,13 @@ class NavigationMapServer:
 
     def __init__(self):
         self.map_pub = rospy.Publisher("navigation_map", OccupancyGrid, queue_size=0, latch=True)
+        self.disk_map_pub = rospy.Publisher("disk_map", OccupancyGrid, queue_size=0)
         self.map_sub = rospy.Subscriber("map", OccupancyGrid, self.map_cb)
         self.lost_sub = rospy.Subscriber("is_lost", Bool, self.lost_cb)
 
         self.is_lost = False
         self.map_msg = OccupancyGrid()
-
+        self.map_pub_count = 0
 
     def map_cb(self, msg):
         # TODO: Stitch maps if lost and update map_msg metadata
@@ -26,10 +28,14 @@ class NavigationMapServer:
         else:
             self.map_msg = msg
 
+        if self.map_pub_count % 5 == 0:  # update map on disk every 5 updates
+            self.disk_map_pub.publish(self.map_msg)
+
         # Set all unknown space as open
         self.map_msg.data = [cell if cell != -1 else 0 for cell in self.map_msg.data]
 
         self.map_pub.publish(self.map_msg)
+        self.map_pub_count += 1
 
     def lost_cb(self, msg):
         self.is_lost = msg.data
