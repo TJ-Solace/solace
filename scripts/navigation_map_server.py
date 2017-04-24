f2676903b6584a337864862866d180159c713d9f
Haicheng Charles Zhao <czhao39@gmail.com>
2017-04-24 12:38:51 -0400

    have navigation map server subscribe to lost detector

--- scripts/navigation_map_server.py
+++ scripts/navigation_map_server.py
@@ -2,6 +2,7 @@
 
 import rospy
 from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import Bool
 
 
 class NavigationMapServer:
@@ -12,16 +13,24 @@ class NavigationMapServer:
     def __init__(self):
         self.map_pub = rospy.Publisher("navigation_map", OccupancyGrid, queue_size=0)
         self.map_sub = rospy.Subscriber("map", OccupancyGrid, self.map_cb)
+        self.lost_sub = rospy.Subscriber("is_lost", Bool, self.lost_cb)
+
+        self.is_lost = False
 
     def map_cb(self, msg):
         altered_map = OccupancyGrid()
 
         # TODO: Stitch maps if lost
+        if self.is_lost:
+            pass
 
         # Set all unknown space as open
         altered_map.data = [cell if cell != -1 else 0 for cell in msg.data]
         
         self.map_pub.publish(altered_map)
+
+    def lost_cb(self, msg):
+        self.is_lost = msg.data
 
 
 if __name__ == "__main__":
