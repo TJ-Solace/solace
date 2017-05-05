e4414ef91a85913e84b2139527d2b7b27cfdbde9
Haicheng Charles Zhao <czhao39@gmail.com>
2017-05-05 08:50:58 -0400

    create separate full map saver and gmapping map saver

--- scripts/navigation_map_server.py
+++ scripts/navigation_map_server.py
@@ -13,7 +13,7 @@ class NavigationMapServer:
 
     def __init__(self):
         self.map_pub = rospy.Publisher("navigation_map", OccupancyGrid, queue_size=0, latch=True)
-        self.disk_map_pub = rospy.Publisher("disk_map", OccupancyGrid, queue_size=0)
+        self.gmapping_disk_map_pub = rospy.Publisher("gmapping_disk_map", OccupancyGrid, queue_size=0)
         self.map_sub = rospy.Subscriber("map", OccupancyGrid, self.map_cb)
         self.lost_sub = rospy.Subscriber("is_lost", Bool, self.lost_cb)
 
@@ -22,11 +22,11 @@ class NavigationMapServer:
         self.lost_count = 0
 
     def map_cb(self, msg):
-        # TODO: Stitch maps if lost and update map_msg metadata
         if self.is_lost:
             self.lost_count += 1
-            if self.lost_count % 5 == 0:  # update map on disk every 5 updates
-                self.disk_map_pub.publish(self.map_msg)
+            if self.lost_count % 3 == 0:  # update map on disk every 3 updates
+                self.gmapping_disk_map_pub.publish(self.map_msg)
+                # TODO: Stitch maps if lost and update map_msg metadata
         else:
             self.map_msg = msg
         actual_map = self.map_msg.data
