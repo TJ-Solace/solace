76ced154154cd9a77a4521830dd018108e5236d0
Haicheng Charles Zhao <czhao39@gmail.com>
2017-05-06 14:39:17 -0400

    fix navigation map server

--- scripts/navigation_map_server.py
+++ scripts/navigation_map_server.py
@@ -20,31 +20,19 @@ class NavigationMapServer:
     def __init__(self):
         self.map_pub = rospy.Publisher("navigation_map", OccupancyGrid, queue_size=0, latch=True)
         self.gmapping_disk_map_pub = rospy.Publisher("gmapping_disk_map", OccupancyGrid, queue_size=0)
-        self.map_sub = rospy.Subscriber("map", OccupancyGrid, self.map_cb)
+        self.gmapping_map_sub = rospy.Subscriber("gmapping_map", OccupancyGrid, self.gmapping_map_cb)
+        self.init_map_sub = rospy.Subscriber("map", OccupancyGrid, self.open_map_cb)
         self.lost_sub = rospy.Subscriber("is_lost", Bool, self.lost_cb)
 
         self.is_lost = False
         self.map_msg = OccupancyGrid()
-        self.lost_count = 0
 
-    def map_cb(self, msg):
-        if self.is_lost:
-            self.lost_count += 1
-            if self.lost_count % 3 == 0:  # update map on disk every 3 updates
-                self.gmapping_disk_map_pub.publish(self.map_msg)  # save gmapping map to disk
-                # stitch gmapping map to full map
-                try:
-                    subprocess.check_call([STITCHING_PATH, FULL_MAP_PATH, GMAPPING_MAP_PATH], stderr=subprocess.STDOUT)
-                    subprocess.call(["convert", "-compress", "none", "out.jpg", FULL_MAP_PATH])
-                    self.map_msg.header.stamp = rospy.Time.now()
-                    self.map_msg.info.map_load_time = rospy.Time.now()
-                    self.file_to_occupancygrid(FULL_MAP_PATH, self.map_msg)
-                    rospy.loginfo("successfully stitched new map!")
-                except subprocess.CalledProcessError:
-                    rospy.logerr("failed to stitch!")
-        else:
-            self.map_msg = msg
-        actual_map = self.map_msg.data
+    def open_map_cb(self, msg):
+        """
+        This function publishes the map with all unknown areas marked as open. 
+        """
+        self.map_msg = msg
+        actual_map = msg.data
 
         # Set all unknown space as open
         self.map_msg.data = [cell if cell != -1 else 0 for cell in self.map_msg.data]
@@ -52,6 +40,23 @@ class NavigationMapServer:
 
         # Revert to actual map
         self.map_msg.data = actual_map
+
+    def gmapping_map_cb(self, msg):
+        if self.is_lost:
+            self.gmapping_disk_map_pub.publish(msg)  # save gmapping map to disk
+            # stitch gmapping map to full map
+            try:
+                subprocess.check_call([STITCHING_PATH, FULL_MAP_PATH, GMAPPING_MAP_PATH], stderr=subprocess.STDOUT)
+                subprocess.call(["convert", "-compress", "none", "out.jpg", FULL_MAP_PATH])
+                self.map_msg.header.stamp = rospy.Time.now()
+                self.map_msg.info.map_load_time = rospy.Time.now()
+                self.file_to_occupancygrid(FULL_MAP_PATH, self.map_msg)
+                rospy.loginfo("successfully stitched new map!")
+            except subprocess.CalledProcessError:
+                rospy.logerr("failed to stitch!")
+            self.open_map_cb(self.map_msg)
+        else:
+            rospy.logerr("why'd I get a gmapping map if I'm not even lost?")
 
     def lost_cb(self, msg):
         self.is_lost = msg.data
