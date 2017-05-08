d76aa8c1a26f114564eb54b3d821f19dee515fe1
Haicheng Charles Zhao <czhao39@gmail.com>
2017-05-08 12:59:02 -0400

    things

--- scripts/physical_control.py
+++ scripts/physical_control.py
@@ -58,10 +58,10 @@ class PhysicalControl():
         self.desired_speed = Float64()
         self.desired_angle = Float64()
         self.current_smoother = ExpSmoother(self.current_input_smoothing_tc)  # try to ignore the reasonably short current transients caused by accelerating sharply
-        self.voltage_smoother = ExpSmoother(self.voltage_input_smoothing_tc, True)  # ignore heavy voltage drop transients from the same
+        self.voltage_smoother = ExpSmoother(self.voltage_input_smoothing_tc)  # ignore heavy voltage drop transients from the same
         self.voltage_smoother.lastSample = 3.8 * 4  # cheat so it doesn't take a long time to become drivable
         self.voltage_smoother.lastRet = 3.8 * 4
-        self.power_smoother = ExpSmoother(self.power_input_smoothing_tc)  # make the stick inputs chill a little bit
+        self.power_smoother = ExpSmoother(self.power_input_smoothing_tc, True)  # make the stick inputs chill a little bit
 
     def command(self, msg):
         thisT = self.get_time(msg.header.stamp)
@@ -71,7 +71,7 @@ class PhysicalControl():
 
     def drive(self, msg):
         thisT = self.get_time(msg.header.stamp)
-        rospy.logdebug_throttle(0.5, "actual speed: " + repr(msg.state.speed))  # just to see
+        rospy.loginfo_throttle(0.5, "actual speed: " + repr(msg.state.speed))  # just to see
         current = self.current_smoother.sample(msg.state.current_motor, thisT)
         voltage = self.voltage_smoother.sample(msg.state.voltage_input, thisT)
         if voltage < self.min_voltage:  # don't get to do anything if the battery is low
