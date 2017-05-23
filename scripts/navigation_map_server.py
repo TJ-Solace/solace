8c02e12383339e4fdc7b1b2711552e013eba082e
Haicheng Charles Zhao <czhao39@gmail.com>
2017-05-22 21:06:10 -0400

    remove unnecessary shell=True

--- scripts/navigation_map_server.py
+++ scripts/navigation_map_server.py
@@ -50,10 +50,10 @@ class NavigationMapServer:
             self.gmapping_disk_map_pub.publish(msg)  # save gmapping map to disk
             # stitch gmapping map to full map
             try:
-	    	rospy.logwarn("{} {} {}".format(STITCHING_PATH, FULL_MAP_PATH, GMAPPING_MAP_PATH))
+	    	rospy.loginfo("{} {} {}".format(STITCHING_PATH, FULL_MAP_PATH, GMAPPING_MAP_PATH))
                 # TODO: remove shell=True if possible
                 subprocess.check_call([STITCHING_PATH, FULL_MAP_PATH, GMAPPING_MAP_PATH], stderr=subprocess.STDOUT, shell=True)
-                subprocess.call(["convert", "-compress", "none", "out.jpg", FULL_MAP_PATH], stderr=subprocess.STDOUT, shell=True)
+                subprocess.call(["convert", "-compress", "none", "out.jpg", FULL_MAP_PATH], stderr=subprocess.STDOUT)
                 self.map_msg.header.stamp = rospy.Time.now()
                 self.map_msg.info.map_load_time = rospy.Time.now()
                 self.file_to_occupancygrid(FULL_MAP_PATH, self.map_msg)
